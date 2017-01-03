e53180a4d9ef7cffb3d924701b4abb91d49ff19b
Henrik Kjellander <kjellander@webrtc.org>
2017-01-03 07:15:02 +0100

    Remove unused SVN setting in codereview.settings.
    BUG=None
    
    Change-Id: I93cf18302cbd9342c47cae110ce00e3a9bdbf67f

--- webrtc/common_types.cc
+++ webrtc/common_types.cc
@@ -7,7 +7,7 @@
  *  in the file PATENTS.  All contributing project authors may
  *  be found in the AUTHORS file in the root of the source tree.
  */
-
+// foo comment.
 #include "webrtc/common_types.h"
 
 #include <limits>
