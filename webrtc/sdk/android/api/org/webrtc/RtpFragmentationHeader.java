3b5f8f06eb49e1e58481b5b5801aea3e2ac75f3d
Sami Kalliomäki <sakal@webrtc.org>
2017-06-02 11:16:34 +0200

    Interfaces for injectable video codecs.
    Change-Id: If199bc2807e1c27094c05983c62fa43d2eec5700

---
+++ webrtc/sdk/android/api/org/webrtc/RtpFragmentationHeader.java
@@ -0,0 +1,26 @@
+/*
+ *  Copyright 2017 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+package org.webrtc;
+
+public class RtpFragmentationHeader {
+  public final long[] fragmentationOffset;
+  public final long[] fragmentationLength;
+  public final int[] fragmentationTimeDiff;
+  public final int[] fragmentationPlType;
+
+  public RtpFragmentationHeader(long[] fragmentationOffset, long[] fragmentationLength,
+      int[] fragmentationTimeDiff, int[] fragmentationPlType) {
+    this.fragmentationOffset = fragmentationOffset;
+    this.fragmentationLength = fragmentationLength;
+    this.fragmentationTimeDiff = fragmentationTimeDiff;
+    this.fragmentationPlType = fragmentationPlType;
+  }
+}
---
+++ webrtc/sdk/android/api/org/webrtc/VideoEncoder.java
@@ -0,0 +1,72 @@
+/*
+ *  Copyright 2017 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+package org.webrtc;
+
+public interface VideoEncoder {
+  // TODO(sakal): Add values to these classes as necessary.
+  public class CodecSpecificInfo {}
+
+  public class CodecSpecificInfoVP8 extends CodecSpecificInfo {}
+
+  public class CodecSpecificInfoVP9 extends CodecSpecificInfo {}
+
+  public class CodecSpecificInfoH264 extends CodecSpecificInfo {}
+
+  public class ScalingSettings {
+    public final boolean on;
+    public final int low;
+    public final int high;
+
+    ScalingSettings(boolean on, int low, int high) {
+      this.on = on;
+      this.low = low;
+      this.high = high;
+    }
+  }
+
+  public class EncodeInfo {
+    public enum FrameType {
+      EmptyFrame,
+      AudioFrameSpeech,
+      AudioFrameCN,
+      VideoFrameKey,
+      VideoFrameDelta,
+    }
+
+    public final FrameType[] frameTypes;
+
+    public EncodeInfo(FrameType[] frameTypes) {
+      this.frameTypes = frameTypes;
+    }
+  }
+
+  public class Settings {
+    public final int numberOfCores;
+
+    public Settings(int numberOfCores) {
+      this.numberOfCores = numberOfCores;
+    }
+  }
+
+  public interface Callback {
+    void onEncodedFrame(EncodedImage frame, CodecSpecificInfo info, RtpFragmentationHeader header);
+  }
+
+  void initEncode(Settings settings, Callback encodeCallback);
+  void release();
+  void encode(VideoFrame frame, EncodeInfo info);
+  void setChannelParameters(int packetLoss, long roundTripTimeMs);
+  void setRateAllocation(BitrateAllocation allocation, long framerate);
+  ScalingSettings getScalingSettings();
+  void setPeriodicKeyFrame(boolean enable);
+  boolean getSupportsNativeHandle();
+  String getImplementationName();
+}
