f5e4fdaf744741f8f47dd2f8581fb856105bc737
minyue-webrtc <minyue@webrtc.org>
2017-07-04 11:30:26 +0200

    Don't use rvalue reference function arguments in the audio coding module
    Rvalue reference arguments are generally banned by the style guide.
    
    Bug:None
    Change-Id: I4314859623ffcf056f53c42087b59696b5e71690

--- webrtc/modules/audio_coding/codecs/opus/audio_encoder_opus.h
+++ webrtc/modules/audio_coding/codecs/opus/audio_encoder_opus.h
@@ -22,6 +22,7 @@
 #include "webrtc/base/constructormagic.h"
 #include "webrtc/base/optional.h"
 #include "webrtc/base/protobuf_utils.h"
+#include "webrtc/base/ptr_util.h"
 #include "webrtc/common_audio/smoothing_filter.h"
 #include "webrtc/modules/audio_coding/audio_network_adaptor/include/audio_network_adaptor.h"
 #include "webrtc/modules/audio_coding/codecs/opus/opus_interface.h"
@@ -67,11 +68,14 @@ class AudioEncoderOpus final : public AudioEncoder {
   // https://bugs.chromium.org/p/webrtc/issues/detail?id=7847
   AudioEncoderOpus(const AudioEncoderOpusConfig& config);
 
+  AudioEncoderOpus(const AudioEncoderOpusConfig& config, int payload_type);
+
+  // Dependency injection for testing.
   AudioEncoderOpus(
       const AudioEncoderOpusConfig& config,
       int payload_type,
-      AudioNetworkAdaptorCreator&& audio_network_adaptor_creator = nullptr,
-      std::unique_ptr<SmoothingFilter> bitrate_smoother = nullptr);
+      const AudioNetworkAdaptorCreator& audio_network_adaptor_creator,
+      std::unique_ptr<SmoothingFilter> bitrate_smoother);
 
   explicit AudioEncoderOpus(const CodecInst& codec_inst);
   AudioEncoderOpus(int payload_type, const SdpAudioFormat& format);
@@ -164,7 +168,7 @@ class AudioEncoderOpus final : public AudioEncoder {
   int next_frame_length_ms_;
   int complexity_;
   std::unique_ptr<PacketLossFractionSmoother> packet_loss_fraction_smoother_;
-  AudioNetworkAdaptorCreator audio_network_adaptor_creator_;
+  const AudioNetworkAdaptorCreator audio_network_adaptor_creator_;
   std::unique_ptr<AudioNetworkAdaptor> audio_network_adaptor_;
   rtc::Optional<size_t> overhead_bytes_per_packet_;
   const std::unique_ptr<SmoothingFilter> bitrate_smoother_;
