0a2f1b3c0bd0b5e32ebb2c49929f7735b067a925
Emad Omara <emadomara@webrtc.org>
2017-07-20 14:56:02 -0700

    Add common TLS extensions
    Bug:
    Change-Id: Ic60e892f0acbe394472319c4d943690828446610

--- webrtc/rtc_base/openssladapter.cc
+++ webrtc/rtc_base/openssladapter.cc
@@ -365,6 +365,11 @@ OpenSSLAdapter::BeginSSL() {
     SSL_set_tlsext_host_name(ssl_, ssl_host_name_.c_str());
   }
 
+  // Set common TLS extensions
+  SSL_enable_ocsp_stapling(ssl_);
+  SSL_enable_signed_cert_timestamps(ssl_);
+  SSL_set_renegotiate_mode(ssl_, ssl_renegotiate_freely);
+
   // the SSL object owns the bio now
   bio = nullptr;
 
