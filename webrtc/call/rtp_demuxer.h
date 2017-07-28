078a86e3ce9965e0dd7e1a0989420e9e3929f492
Steve Anton <steveanton@webrtc.org>
2017-07-27 18:01:33 -0700

    Add MID demuxing to RtpDemuxer.
    Can now create MID sinks which will be mapped when an RTP packet with the
    MID extension is received. The SSRC->MID mapping is also created at the
    same time so that future packets without the MID extension will also be
    routed to the sink.
    
    Depends on https://chromium-review.googlesource.com/c/576374/
    
    Bug: webrtc:4050
    Change-Id: Ie2f347f90d5074ab537fa1162fa7314dd292b68b

--- webrtc/call/rtp_demuxer.h
+++ webrtc/call/rtp_demuxer.h
@@ -13,7 +13,10 @@
 
 #include <map>
 #include <string>
+#include <utility>
 #include <vector>
+
+#include "webrtc/rtc_base/constructormagic.h"
 
 namespace webrtc {
 
@@ -21,15 +24,49 @@ class RtpPacketReceived;
 class RtpPacketSinkInterface;
 class SsrcBindingObserver;
 
+// This struct describes the criteria that will be used to match packets to a
+// specific sink.
+struct RtpDemuxerCriteria {
+  RtpDemuxerCriteria();
+  ~RtpDemuxerCriteria();
+
+  // If not the empty string, will match packets with this MID.
+  std::string mid;
+
+  // Will match packets with any of these SSRCs.
+  std::vector<uint32_t> ssrcs;
+
+  // Will match packets with any of these payload types.
+  std::vector<uint8_t> payload_types;
+
+  // Will match packets with any of these RTP stream IDs. If MID is also
+  // specified, will match RSIDs scoped within the MID.
+  std::vector<std::string> rsids;
+};
+
 // This class represents the RTP demuxing, for a single RTP session (i.e., one
 // ssrc space, see RFC 7656). It isn't thread aware, leaving responsibility of
 // multithreading issues to the user of this class.
-// TODO(nisse): Should be extended to also do MID-based demux and payload-type
-// demux.
+// The demuxing algorithm follows the sketch given in the BUNDLE draft:
+// https://tools.ietf.org/html/draft-ietf-mmusic-sdp-bundle-negotiation-38#section-10.2
+// with additional support for RTP stream IDs.
 class RtpDemuxer {
  public:
   RtpDemuxer();
   ~RtpDemuxer();
+
+  RtpDemuxer(const RtpDemuxer&) = delete;
+  void operator=(const RtpDemuxer&) = delete;
+
+  // Registers a sink that will be notified when RTP packets uniquely match the
+  // given criteria.
+  // Returns true if the sink was successfully added.
+  // Returns false in the following situations:
+  // - Any of the criteria SSRCs are already registered.
+  // - Any of the MID, RSID pairs are already registered.
+  // If false is returned, no changes are made to the demuxer state.
+  bool AddSink(const RtpDemuxerCriteria& criteria,
+               RtpPacketSinkInterface* sink);
 
   // Registers a sink. Multiple SSRCs may be mapped to the same sink, but
   // each SSRC may only be mapped to one sink. The return value reports
@@ -57,21 +94,47 @@ class RtpDemuxer {
   void DeregisterSsrcBindingObserver(const SsrcBindingObserver* observer);
 
  private:
-  // Find the associations of RSID to SSRCs.
-  void ResolveRsidToSsrcAssociations(const RtpPacketReceived& packet);
+  // Returns true if adding a sink with the given criteria would cause conflicts
+  // with the existing criteria and should be prevented.
+  bool CriteriaWouldConflict(const RtpDemuxerCriteria& criteria) const;
+
+  // Returns the sink that is bound to the given SSRC, or null if not found.
+  RtpPacketSinkInterface* GetSinkBySsrc(uint32_t ssrc) const;
+
+  // Runs the demux algorithm on the given packet and returns the sink that
+  // should receive the packet.
+  // If the packet should be dropped, this method returns null.
+  RtpPacketSinkInterface* FindSink(const RtpPacketReceived& packet);
+
+  // If the second element of the returned pair is true, processing should stop
+  // and the packet should be dropped.
+  std::pair<RtpPacketSinkInterface*, bool> FindSinkByMid(
+      const RtpPacketReceived& packet);
+  RtpPacketSinkInterface* FindSinkByPayloadType(
+      const RtpPacketReceived& packet);
+  RtpPacketSinkInterface* FindSinkByRsid(const RtpPacketReceived& packet);
 
   // Notify observers of the resolution of an RSID to an SSRC.
   void NotifyObserversOfRsidResolution(const std::string& rsid, uint32_t ssrc);
 
-  // This records the association SSRCs to sinks. Other associations, such
-  // as by RSID, also end up here once the RSID, etc., is resolved to an SSRC.
-  std::map<uint32_t, RtpPacketSinkInterface*> ssrc_sinks_;
-
-  // A sink may be associated with an RSID - RTP Stream ID. This tag has a
-  // one-to-one association with an SSRC, but that SSRC is not yet known.
-  // When it becomes known, the association of the sink to the RSID is deleted
-  // from this container, and moved into |ssrc_sinks_|.
-  std::map<std::string, RtpPacketSinkInterface*> rsid_sinks_;
+  // Mappings for each attribute from values to the sinks that have that value
+  // as their criteria.
+  // MID and Payload Type mappings are multimap because it's possible for
+  // different criteria to share attributes. If multiple sinks are associated
+  // to a value, the demuxing algorithm will not use that attribute alone to
+  // choose the receiving sink.
+  // Note: Mappings are only modified by AddSink/RemoveSink (except for
+  // SSRC mapping which receives all MID, payload type, or RSID to SSRC bindings
+  // discovered when demuxing packets).
+  std::multimap<std::string, RtpPacketSinkInterface*> sinks_by_mid_;
+  // Note: SSRC bindings are unique.
+  std::map<uint32_t, RtpPacketSinkInterface*> sinks_by_ssrc_;
+  std::multimap<uint8_t, RtpPacketSinkInterface*> sinks_by_payload_type_;
+  // Maps (MID, SSRC) --> Sink.
+  // RSIDs are scoped within MIDs, but using RSID does require using MID.
+  // MID value of empty string indicates RSID alone, no MID.
+  std::map<std::pair<std::string, std::string>, RtpPacketSinkInterface*>
+      sinks_by_mid_rsid_;
 
   // Observers which will be notified when an RSID association to an SSRC is
   // resolved by this object.
