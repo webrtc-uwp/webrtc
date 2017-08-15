6c5fcb3a0afdee5ded662442d156e275ea5590a0
Zijie He <zijiehe@chromium.org>
2017-08-14 19:48:20 -0700

    Share ScreenDrawerLockPosix implementation
    This change renames ScreenDrawerLockLinux into ScreenDrawerLockPosix and shares
    it with Mac OSX.
    
    Bug: webrtc:7950
    Change-Id: Ib141781d2c35bfda0d6f9458fff235adbb643280

--- webrtc/modules/desktop_capture/screen_drawer_unittest.cc
+++ webrtc/modules/desktop_capture/screen_drawer_unittest.cc
@@ -10,15 +10,77 @@
 
 #include "webrtc/modules/desktop_capture/screen_drawer.h"
 
+#include <atomic>
 #include <stdint.h>
 
+#include "webrtc/rtc_base/checks.h"
+#include "webrtc/rtc_base/function_view.h"
 #include "webrtc/rtc_base/logging.h"
 #include "webrtc/rtc_base/random.h"
+#include "webrtc/rtc_base/platform_thread.h"
+#include "webrtc/rtc_base/ptr_util.h"
 #include "webrtc/rtc_base/timeutils.h"
 #include "webrtc/system_wrappers/include/sleep.h"
 #include "webrtc/test/gtest.h"
 
+#if defined(WEBRTC_POSIX)
+#include "webrtc/modules/desktop_capture/screen_drawer_lock_posix.h"
+#endif
+
 namespace webrtc {
+
+namespace {
+
+void TestScreenDrawerLock(
+    rtc::FunctionView<std::unique_ptr<ScreenDrawerLock>()> ctor) {
+  RTC_DCHECK(ctor);
+
+  const int64_t start_ms = rtc::TimeMillis();
+  std::atomic<bool> created(false);
+
+  class Task {
+   public:
+    Task(std::atomic<bool>* created,
+         rtc::FunctionView<std::unique_ptr<ScreenDrawerLock>()> ctor)
+        : created_(created),
+          ctor_(ctor) {}
+
+    ~Task() = default;
+
+    static void RunTask(void* me) {
+      Task* task = static_cast<Task*>(me);
+      std::unique_ptr<ScreenDrawerLock> lock = task->ctor_();
+      task->created_->store(true);
+      const int64_t current_ms = rtc::TimeMillis();
+      while (rtc::TimeMillis() - current_ms < 100) {
+        SleepMs(100 - (rtc::TimeMillis() - current_ms));
+      }
+    }
+
+   private:
+    std::atomic<bool>* const created_;
+    const rtc::FunctionView<std::unique_ptr<ScreenDrawerLock>()> ctor_;
+  } task(&created, ctor);
+
+  rtc::PlatformThread lock_thread(&Task::RunTask, &task, "lock_thread");
+  lock_thread.Start();
+
+  // CriticalSection cannot be released in a different thread, which triggers
+  // "mutex was not held" error.
+  // Event does not implemented for all platforms.
+  // So fallback to use a manual loop.
+  // TODO(zijiehe): Find a better solution to wait for the creation of the first
+  // lock.
+  while (!created.load()) {
+    SleepMs(10);
+  }
+
+  ctor();
+  ASSERT_LE(100, rtc::TimeMillis() - start_ms);
+  lock_thread.Stop();
+}
+
+}  // namespace
 
 // These are a set of manual test cases, as we do not have an automatical way to
 // detect whether a ScreenDrawer on a certain platform works well without
@@ -55,6 +117,34 @@ TEST(ScreenDrawerTest, DISABLED_DrawRectangles) {
   }
 
   SleepMs(10000);
+}
+
+TEST(ScreenDrawerTest, TwoScreenDrawerLocks) {
+#if defined(WEBRTC_POSIX)
+  // ScreenDrawerLockPosix won't be able to unlink the named semaphore. So use a
+  // different semaphore name here to avoid deadlock.
+  const char* semaphore_name = "GSDL8784541a812011e788ff67427b";
+  ScreenDrawerLockPosix::Unlink(semaphore_name);
+
+  TestScreenDrawerLock([semaphore_name]() {
+    return rtc::MakeUnique<ScreenDrawerLockPosix>(semaphore_name);
+  });
+#else
+  // ScreenDrawerLock may not be implemented for all platforms: check its
+  // availability first.
+  {
+    std::unique_ptr<ScreenDrawerLock> lock = ScreenDrawerLock::Create();
+    if (!lock) {
+      LOG(LS_WARNING) <<
+          "No ScreenDrawerLock implementation for current platform.";
+      return;
+    }
+  }
+
+  TestScreenDrawerLock([]() {
+    return ScreenDrawerLock::Create();
+  });
+#endif
 }
 
 }  // namespace webrtc
