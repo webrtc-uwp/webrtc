f744f534bbf626af5c11e32acc21a86a913e76c4
Elad Alon <eladalon@webrtc.org>
2017-09-22 10:36:19 +0200

    Modularize RtcEventLog's output
    By making RtcEventLog's output modular, we allow it to either go directly to a file (current behavior), or be passed to the hosting application (such as Chrome).
    
    Bug:webrtc:8111
    Change-Id: I84d6cc9c93ec2aa441c80cc1985a14dc55cb5f48

---
+++ logging/rtc_event_log/output/rtc_event_log_output_file_unittest.cc
@@ -0,0 +1,98 @@
+/*
+ *  Copyright (c) 2017 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include <fstream>
+#include <memory>
+#include <string>
+
+#include "logging/rtc_event_log/output/rtc_event_log_output_file.h"
+#include "rtc_base/ptr_util.h"
+#include "rtc_base/random.h"
+#include "system_wrappers/include/clock.h"
+#include "test/gtest.h"
+#include "test/testsupport/fileutils.h"
+
+namespace webrtc {
+
+class RtcEventLogOutputFileTest : public ::testing::Test {
+ public:
+  ~RtcEventLogOutputFileTest() override = default;
+
+ private:
+};
+
+TEST_F(RtcEventLogOutputFileTest, UnlimitedOutputFile) {
+  // Random input to increase confidence that tests don't pass because of old
+  // temporary files that failed to be deleted and/or overwritten.
+  Random prng(Clock::GetRealTimeClock()->TimeInMicroseconds());
+
+  auto test_info = ::testing::UnitTest::GetInstance()->current_test_info();
+  const std::string temp_filename =
+      test::OutputPath() + test_info->test_case_name() + test_info->name();
+  remove(temp_filename.c_str());  // Ensure no leftovers from previous runs.
+
+  std::stringstream output_stringstream;
+  for (size_t i = 0; i < 1000; i++) {
+    output_stringstream << std::to_string(prng.Rand(0, 9));
+  }
+  std::string output_str = output_stringstream.str();
+
+  auto output_file = rtc::MakeUnique<RtcEventLogOutputFile>(temp_filename);
+  output_file->Write(output_str);
+  output_file.reset();  // Closing the file flushes the buffer to disk.
+
+  std::ifstream file(temp_filename, std::ios_base::in | std::ios_base::binary);
+  ASSERT_TRUE(file.good());
+  ASSERT_TRUE(file.is_open());
+  std::string file_str((std::istreambuf_iterator<char>(file)),
+                       std::istreambuf_iterator<char>());
+
+  EXPECT_EQ(output_str, file_str);
+
+  remove(temp_filename.c_str());
+}
+
+TEST_F(RtcEventLogOutputFileTest, LimitedOutputFile) {
+  constexpr size_t file_size_limit = 50;
+
+  // Random input to increase confidence that tests don't pass because of old
+  // temporary files that failed to be deleted and/or overwritten.
+  Random prng(Clock::GetRealTimeClock()->TimeInMicroseconds());
+
+  auto test_info = ::testing::UnitTest::GetInstance()->current_test_info();
+  const std::string temp_filename =
+      test::OutputPath() + test_info->test_case_name() + test_info->name();
+  remove(temp_filename.c_str());  // Ensure no leftovers from previous runs.
+
+  std::stringstream output_stringstream;
+  for (size_t i = 0; i < 2 * file_size_limit; i++) {
+    output_stringstream << std::to_string(prng.Rand(0, 9));
+  }
+  std::string output_str = output_stringstream.str();
+
+  auto output_file = rtc::MakeUnique<RtcEventLogOutputFile>(temp_filename,
+                                                            file_size_limit);
+  ASSERT_TRUE(output_file->Write(output_str.substr(0, file_size_limit)));
+  ASSERT_FALSE(output_file->Write(output_str.substr(file_size_limit,
+                                                    2 * file_size_limit)));
+  output_file.reset();  // Closing the file flushes the buffer to disk.
+
+  std::ifstream file(temp_filename, std::ios_base::in | std::ios_base::binary);
+  ASSERT_TRUE(file.good());
+  ASSERT_TRUE(file.is_open());
+  std::string file_str((std::istreambuf_iterator<char>(file)),
+                       std::istreambuf_iterator<char>());
+
+  EXPECT_EQ(output_str.substr(0, file_size_limit), file_str);
+
+  remove(temp_filename.c_str());
+}
+
+}  // namespace webrtc
