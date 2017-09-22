91be58e384eff2c466c533d4cf54f0d3344eed1e
Niels Möller <nisse@webrtc.org>
2017-09-22 14:12:05 +0200

    New classes RefCounter and RefCountedBase.
    Bug: webrtc:8270
    Change-Id: Ibdab81b3fcbe6cba9ae24033f56c84b13c868b21

---
+++ api/refcountedbase.h
@@ -0,0 +1,36 @@
+/*
+ *  Copyright 2017 The WebRTC Project Authors. All rights reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+#ifndef API_REFCOUNTEDBASE_H_
+#define API_REFCOUNTEDBASE_H_
+
+#include "rtc_base/refcounter.h"
+
+// TODO(nisse): Or webrtc?
+namespace rtc {
+
+class RefCountedBase {
+ public:
+  void AddRef() const { ref_count_.AddRef(); }
+  // TODO(nisse): Return void or bool?
+  void Release() const {
+    bool dead = (ref_count_.Release() == 0);
+    if (dead) {
+      delete this;
+    }
+  }
+
+  // TODO(nisse): Include HasOneRef?
+ private:
+  mutable webrtc::webrtc_impl::RefCounter ref_count_;
+};
+
+}  // namespace rtc
+
+#endif  // API_REFCOUNTEDBASE_H_
