24fd93d923389be972c4f27e9f5f3134c41bb378
Anders Carlsson <andersc@webrtc.org>
2017-09-27 15:03:24 +0200

    Injectable software video codecs in Obj-C.
    When injecting video codec factories in the Obj-C SDK, use the new
    peer connection API that uses webrtc::Video{De,En}CoderFactory classes
    and does not automatically add internal software codecs. Instead the
    injected factory can support internal VP8 and VP9 codecs through the
    included Obj-C classes RTCVideo{De,En}coderVP{8,9}.
    
    When not explicitly injecting any video codec factory, the old code
    path is still used and injects only H264 as an external codec and
    the internal codec factory is used.
    
    Bug: webrtc:7925
    Change-Id: I657d30dfde71da9c0be341e213ab9f97a04caa58

---
+++ sdk/objc/Framework/Classes/PeerConnection/RTCInternalSoftwareVideoEncoder.mm
@@ -0,0 +1,133 @@
+/*
+ *  Copyright (c) 2017 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#import <Foundation/Foundation.h>
+
+#import "NSString+StdString.h"
+#import "RTCInternalSoftwareVideoEncoder+Private.h"
+#import "RTCVideoCodec+Private.h"
+
+#include "media/engine/internalencoderfactory.h"
+#include "rtc_base/timeutils.h"
+#include "sdk/objc/Framework/Classes/Video/objc_frame_buffer.h"
+
+const size_t kDefaultPayloadSize = 1440;
+
+class WrappedEncodedImageCallback : public webrtc::EncodedImageCallback {
+ public:
+  Result OnEncodedImage(const webrtc::EncodedImage &encoded_image,
+                        const webrtc::CodecSpecificInfo *codec_specific_info,
+                        const webrtc::RTPFragmentationHeader *fragmentation) {
+    RTCEncodedImage *image = [[RTCEncodedImage alloc] initWithNativeEncodedImage:encoded_image];
+    id<RTCCodecSpecificInfo> info = nil;
+    RTCRtpFragmentationHeader *header =
+        [[RTCRtpFragmentationHeader alloc] initWithNativeFragmentationHeader:fragmentation];
+    callback(image, info, header);
+    return Result(Result::OK, 0);
+  }
+
+  RTCVideoEncoderCallback callback;
+};
+
+@implementation RTCInternalSoftwareVideoEncoder {
+  std::unique_ptr<webrtc::VideoEncoder> _wrappedEncoder;
+
+  WrappedEncodedImageCallback *_encodedImageCallback;
+}
+
++ (NSSet<NSString *> *)supportedFormats {
+  return [NSSet setWithObjects:@"VP8", @"VP9", nil];
+}
+
+- (instancetype)initWithVideoCodec:(const cricket::VideoCodec &)codec {
+  if (self = [super init]) {
+    cricket::InternalEncoderFactory internal_factory;
+    std::unique_ptr<webrtc::VideoEncoder> encoder(internal_factory.CreateVideoEncoder(codec));
+    RTC_CHECK(encoder);
+
+    _wrappedEncoder = std::move(encoder);
+  }
+
+  return self;
+}
+
+- (std::unique_ptr<webrtc::VideoEncoder>)wrappedEncoder {
+  return std::move(_wrappedEncoder);
+}
+
+- (void)dealloc {
+  if (_encodedImageCallback) {
+    delete _encodedImageCallback;
+  }
+}
+
+#pragma mark - RTCVideoEncoder
+
+- (void)setCallback:(RTCVideoEncoderCallback)callback {
+  _encodedImageCallback = new WrappedEncodedImageCallback();
+  _encodedImageCallback->callback = callback;
+  _wrappedEncoder->RegisterEncodeCompleteCallback(_encodedImageCallback);
+}
+
+- (NSInteger)startEncodeWithSettings:(RTCVideoEncoderSettings *)settings
+                       numberOfCores:(int)numberOfCores {
+  webrtc::VideoCodec codecSettings = [settings nativeVideoCodec];
+  return _wrappedEncoder->InitEncode(&codecSettings, numberOfCores, kDefaultPayloadSize);
+}
+
+- (NSInteger)releaseEncoder {
+  return _wrappedEncoder->Release();
+}
+
+- (NSInteger)encode:(RTCVideoFrame *)frame
+    codecSpecificInfo:(id<RTCCodecSpecificInfo>)info
+           frameTypes:(NSArray<NSNumber *> *)frameTypes {
+  rtc::scoped_refptr<webrtc::VideoFrameBuffer> frameBuffer =
+      new rtc::RefCountedObject<webrtc::ObjCFrameBuffer>(frame.buffer);
+  webrtc::VideoFrame videoFrame(frameBuffer,
+                                (webrtc::VideoRotation)frame.rotation,
+                                frame.timeStampNs / rtc::kNumNanosecsPerMicrosec);
+  videoFrame.set_timestamp(frame.timeStamp);
+
+  // Handle types that can be converted into one of webrtc::CodecSpecificInfo's hard coded cases.
+  webrtc::CodecSpecificInfo codecSpecificInfo;
+  if ([info isKindOfClass:[RTCCodecSpecificInfoH264 class]]) {
+    codecSpecificInfo = [(RTCCodecSpecificInfoH264 *)info nativeCodecSpecificInfo];
+  }
+
+  std::vector<webrtc::FrameType> nativeFrameTypes;
+  for (NSNumber *frameType in frameTypes) {
+    RTCFrameType rtcFrameType = (RTCFrameType)frameType.unsignedIntegerValue;
+    nativeFrameTypes.push_back((webrtc::FrameType)rtcFrameType);
+  }
+
+  return _wrappedEncoder->Encode(videoFrame, &codecSpecificInfo, &nativeFrameTypes);
+}
+
+- (int)setBitrate:(uint32_t)bitrateKbit framerate:(uint32_t)framerate {
+  return _wrappedEncoder->SetRates(bitrateKbit, framerate);
+}
+
+- (NSString *)implementationName {
+  return [NSString stringForStdString:_wrappedEncoder->ImplementationName()];
+}
+
+- (RTCVideoEncoderQpThresholds *)scalingSettings {
+  rtc::Optional<webrtc::VideoEncoder::QpThresholds> thresholds =
+      _wrappedEncoder->GetScalingSettings().thresholds;
+  if (thresholds.has_value()) {
+    return [[RTCVideoEncoderQpThresholds alloc] initWithThresholdsLow:thresholds->low
+                                                                 high:thresholds->high];
+  } else {
+    return nil;
+  }
+}
+
+@end
