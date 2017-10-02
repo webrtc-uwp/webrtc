8f0a3e1f1621c55f3af728de79f81af933c30457
Kári Tristan Helgason <kthelgason@webrtc.org>
2017-10-02 20:14:11 +0200

    Add tests for ScopedCFTypeRef.
    Copy the chromium pattern of taking Traits as template args to be
    able to write a testable implementation of ScopedTypeRef.
    
    Add unit tests for th current implementation.
    
    Bug: webrtc:7825
    Change-Id: I26017952c042c8323b9c3841ec309e32d1c04a85

---
+++ sdk/objc/Framework/UnitTests/scoped_cftyperef_tests.mm
@@ -0,0 +1,99 @@
+/*
+ *  Copyright 2017 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include "sdk/objc/Framework/Classes/Common/scoped_cftyperef.h"
+
+#include "test/gtest.h"
+
+struct TestType {
+  TestType() : has_value(true) {}
+  TestType(bool b) : has_value(b) {}
+  operator bool() { return has_value; }
+  bool has_value;
+  int retain_count = 0;
+};
+
+typedef TestType* TestTypeRef;
+
+struct TestTypeTraits {
+  static TestTypeRef InvalidValue() { return TestTypeRef(false); }
+  static void Release(TestTypeRef t) { t->retain_count--; }
+  static TestTypeRef Retain(TestTypeRef t) {
+    t->retain_count++;
+    return t;
+  }
+};
+
+using ScopedTestType = rtc::internal::ScopedTypeRef<TestTypeRef, TestTypeTraits>;
+using rtc::RetainPolicy;
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunused-variable"
+
+TEST(ScopedTypeRefTest, ShouldNotRetainByDefault) {
+  TestTypeRef a = new TestType();
+  ScopedTestType ref(a);
+  ASSERT_EQ(0, a->retain_count);
+}
+
+TEST(ScopedTypeRefTest, ShouldRetainWithPolicy) {
+  TestTypeRef a = new TestType();
+  ScopedTestType ref(a, RetainPolicy::RETAIN);
+  ASSERT_EQ(1, a->retain_count);
+}
+
+TEST(ScopedTypeRefTest, ShouldReleaseWhenLeavingScope) {
+  TestTypeRef a = new TestType();
+  ASSERT_EQ(0, a->retain_count);
+  {
+    ScopedTestType ref(a, RetainPolicy::RETAIN);
+    ASSERT_EQ(1, a->retain_count);
+  }
+  ASSERT_EQ(0, a->retain_count);
+}
+
+TEST(ScopedTypeRefTest, ShouldBeCopyable) {
+  TestTypeRef a = new TestType();
+  ASSERT_EQ(0, a->retain_count);
+  {
+    ScopedTestType ref1(a, RetainPolicy::RETAIN);
+    ASSERT_EQ(1, a->retain_count);
+    ScopedTestType ref2 = ref1;
+    ASSERT_EQ(2, a->retain_count);
+  }
+  ASSERT_EQ(0, a->retain_count);
+}
+
+TEST(ScopedTypeRefTest, CanReleaseOwnership) {
+  TestTypeRef a = new TestType();
+  ASSERT_EQ(0, a->retain_count);
+  {
+    ScopedTestType ref(a, RetainPolicy::RETAIN);
+    ASSERT_EQ(1, a->retain_count);
+    TestTypeRef b = ref.release();
+  }
+  ASSERT_EQ(1, a->retain_count);
+}
+
+TEST(ScopedTypeRefTest, ShouldBeTestableForTruthiness) {
+  ScopedTestType ref;
+  ASSERT_FALSE(ref);
+  TestTypeRef a = new TestType();
+  ref = a;
+  ASSERT_TRUE(ref);
+}
+
+TEST(ScopedTypeRefTest, ShouldProvideAccessToWrappedType) {
+  TestTypeRef a = new TestType();
+  ScopedTestType ref(a);
+  ASSERT_EQ(&(a->retain_count), &(ref->retain_count));
+}
+
+#pragma clang diagnostic pop
