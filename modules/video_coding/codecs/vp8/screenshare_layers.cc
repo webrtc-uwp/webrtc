cc989d3806829ee850318b2b6f5f1eab3a6bc74b
Ilya Nikolaevskiy <ilnik@webrtc.org>
2017-10-02 13:01:34 +0200

    Implement ScreenshareTemporalLayersChecker
    Bug:none
    Change-Id: Ic95156d0f47d186e2289264aa9a916511a8e4510

--- modules/video_coding/codecs/vp8/screenshare_layers.cc
+++ modules/video_coding/codecs/vp8/screenshare_layers.cc
@@ -16,6 +16,7 @@
 
 #include "modules/video_coding/include/video_codec_interface.h"
 #include "rtc_base/checks.h"
+#include "rtc_base/logging.h"
 #include "system_wrappers/include/clock.h"
 #include "system_wrappers/include/metrics.h"
 #include "vpx/vp8cx.h"
@@ -476,14 +477,75 @@ void ScreenshareLayers::UpdateHistograms() {
 }
 
 ScreenshareTemporalLayersChecker::ScreenshareTemporalLayersChecker(
-    int /*num_temporal_layers*/,
-    uint8_t /*initial_tl0_pic_idx*/) {}
+    int num_temporal_layers,
+    uint8_t /*initial_tl0_pic_idx*/)
+    : num_temporal_layers_(num_temporal_layers) {}
 
-// TODO(ilnik): Implement layers dependency checks here. Keep track of
-// last/golden/arf buffers and sync bits.
+bool ScreenshareTemporalLayersChecker::CheckAndUpdateBufferState(
+    BufferState* state,
+    bool* need_sync,
+    bool frame_is_keyframe,
+    uint8_t temporal_layer,
+    webrtc::TemporalLayers::BufferFlags flags) {
+  if (flags & TemporalLayers::BufferFlags::kReference) {
+    if (state->temporal_layer > 0) {
+      *need_sync = false;
+    }
+    if (!frame_is_keyframe && !state->is_keyframe &&
+        state->temporal_layer > temporal_layer) {
+      LOG(LS_ERROR) << "Frame is referencing higher temporal layer.";
+      return false;
+    }
+  }
+  if (flags & TemporalLayers::BufferFlags::kUpdate) {
+    state->temporal_layer = temporal_layer;
+    state->is_keyframe = frame_is_keyframe;
+  }
+  return true;
+}
+
 bool ScreenshareTemporalLayersChecker::CheckTemporalConfig(
-    bool /*frame_is_keyframe*/,
-    const TemporalLayers::FrameConfig& /*frame_config*/) {
+    bool frame_is_keyframe,
+    const TemporalLayers::FrameConfig& frame_config) {
+  if (frame_config.drop_frame) {
+    return true;
+  }
+  if (frame_config.packetizer_temporal_idx >= num_temporal_layers_ ||
+      (frame_config.packetizer_temporal_idx == kNoTemporalIdx &&
+       num_temporal_layers_ > 1)) {
+    LOG(LS_ERROR) << "Incorrect temporal layer set for frame: "
+                  << frame_config.packetizer_temporal_idx
+                  << " num_temporal_layers: " << num_temporal_layers_;
+    return false;
+  }
+
+  bool need_sync = frame_config.packetizer_temporal_idx > 0 &&
+                   frame_config.packetizer_temporal_idx != kNoTemporalIdx;
+
+  if (!CheckAndUpdateBufferState(&last_, &need_sync, frame_is_keyframe,
+                                 frame_config.packetizer_temporal_idx,
+                                 frame_config.last_buffer_flags)) {
+    LOG(LS_ERROR) << "Error in the Last buffer";
+    return false;
+  }
+  if (!CheckAndUpdateBufferState(&golden_, &need_sync, frame_is_keyframe,
+                                 frame_config.packetizer_temporal_idx,
+                                 frame_config.golden_buffer_flags)) {
+    LOG(LS_ERROR) << "Error in the Golden buffer";
+    return false;
+  }
+  if (!CheckAndUpdateBufferState(&arf_, &need_sync, frame_is_keyframe,
+                                 frame_config.packetizer_temporal_idx,
+                                 frame_config.arf_buffer_flags)) {
+    LOG(LS_ERROR) << "Error in the Arf buffer";
+    return false;
+  }
+
+  if (need_sync != frame_config.layer_sync) {
+    LOG(LS_ERROR) << "Sync bit is set incorrectly on a frame. Expected: "
+                  << need_sync << " Actual: " << frame_config.layer_sync;
+    return false;
+  }
   return true;
 }
 
