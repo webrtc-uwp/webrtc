76e46284a68c63741804bc8e00c6b031e34481e1
Edward Lemur <ehmaldonado@webrtc.org>
2017-10-04 16:46:26 +0200

    Ignore unknown arguments in low_bandwidth_audio_test.py
    Bug: chromium:755660
    Change-Id: Iff9fb3441200f760c511a67211fbc4a1272717b4

--- audio/test/low_bandwidth_audio_test.py
+++ audio/test/low_bandwidth_audio_test.py
@@ -54,7 +54,7 @@ def _ParseArgs():
   parser.add_argument('--android', action='store_true',
       help='Perform the test on a connected Android device instead.')
   parser.add_argument('--adb-path', help='Path to adb binary.', default='adb')
-  args = parser.parse_args()
+  args, _ = parser.parse_known_args()
   return args
 
 
