18309a648c45e759d84e4aecc938fb5cf46f8487
Per Åhgren <peah@webrtc.org>
2018-03-15 13:50:46 +0100

    Merge to M66: Separating the AEC3 suppressor gain rampup behavior for...
    This CL introduces a different rampup behavir for the call startup and after resets
    that may occur due to delay changes, clock-drift and audio path glitches.
    
    (cherry picked from commit 12eb85881c865a67be07d04856383fea22890d6a)
    
    TBR: gustaf@webrtc.org,devicentepena@webrtc.org,henrik.lundin@webrtc.org
    Bug: chromium:819111, webrtc:8979
    Change-Id: Ied1d7896be7f0c69aa6deb61475117021ca6ab09
    Reviewed-on: https://webrtc-review.googlesource.com/60002
    Reviewed-by: Gustaf Ullberg <gustaf@webrtc.org>
    Reviewed-by: Jesus de Vicente Pena <devicentepena@webrtc.org>
    Commit-Queue: Per Åhgren <peah@webrtc.org>
    Cr-Original-Commit-Position: refs/heads/master@{#22312}
    Reviewed-on: https://webrtc-review.googlesource.com/62061
    Reviewed-by: Per Åhgren <peah@webrtc.org>
    Cr-Commit-Position: refs/branch-heads/66@{#7}
    Cr-Branched-From: 12c8110e8c717b7f0f87615d3b99caac2a69fa6c-refs/heads/master@{#22215}

--- modules/audio_processing/aec3/aec_state.h
+++ modules/audio_processing/aec3/aec_state.h
@@ -24,6 +24,7 @@
 #include "modules/audio_processing/aec3/erl_estimator.h"
 #include "modules/audio_processing/aec3/erle_estimator.h"
 #include "modules/audio_processing/aec3/render_buffer.h"
+#include "modules/audio_processing/aec3/suppression_gain_limiter.h"
 #include "rtc_base/constructormagic.h"
 
 namespace webrtc {
@@ -89,7 +90,9 @@ class AecState {
   float ReverbDecay() const { return reverb_decay_; }
 
   // Returns the upper limit for the echo suppression gain.
-  float SuppressionGainLimit() const { return suppressor_gain_limit_; }
+  float SuppressionGainLimit() const {
+    return suppression_gain_limiter_.Limit();
+  }
 
   // Returns whether the echo in the capture signal is audible.
   bool InaudibleEcho() const { return echo_audibility_.InaudibleEcho(); }
@@ -154,9 +157,6 @@ class AecState {
   bool transparent_mode_ = false;
   float previous_max_sample_ = 0.f;
   bool render_received_ = false;
-  int realignment_counter_ = 0;
-  float suppressor_gain_limit_ = 1.f;
-  bool active_render_seen_ = false;
   int filter_delay_ = 0;
   size_t blocks_since_last_saturation_ = 1000;
   float reverb_decay_to_test_ = 0.9f;
@@ -170,6 +170,7 @@ class AecState {
   bool filter_has_had_time_to_converge_ = false;
   bool initial_state_ = true;
   const float gain_rampup_increase_;
+  SuppressionGainUpperLimiter suppression_gain_limiter_;
 
   RTC_DISALLOW_COPY_AND_ASSIGN(AecState);
 };
