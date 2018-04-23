d8831bbac61cc49f957c702af6f9e66247708938
Steve Anton <steveanton@webrtc.org>
2018-04-23 23:42:10 +0000

    Revert "Fix crash when GetTransportStatsByNames called after closing"
    This reverts commit d84d4abf1f0e1626c77a17d28fd5b25498feab82.
    
    Reason for revert: canary first
    
    Original change's description:
    > Fix crash when GetTransportStatsByNames called after closing
    > 
    > Bug: chromium:829238
    > Change-Id: Ifaa3d53e969dae2200ab49e34b25897dbb5cb507
    > Reviewed-on: https://webrtc-review.googlesource.com/71940
    > Reviewed-by: Qingsi Wang <qingsi@webrtc.org>
    > Cr-Commit-Position: refs/branch-heads/66@{#21}
    > Cr-Branched-From: 12c8110e8c717b7f0f87615d3b99caac2a69fa6c-refs/heads/master@{#22215}
    
    TBR=steveanton@webrtc.org,qingsi@webrtc.org
    
    Change-Id: Ie9afdf4520cc1adcfaf0ecc47abaf3c0e318bfa3
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: chromium:829238
    Reviewed-on: https://webrtc-review.googlesource.com/71960
    Reviewed-by: Steve Anton <steveanton@webrtc.org>
    Cr-Commit-Position: refs/branch-heads/66@{#22}
    Cr-Branched-From: 12c8110e8c717b7f0f87615d3b99caac2a69fa6c-refs/heads/master@{#22215}

--- pc/peerconnection.cc
+++ pc/peerconnection.cc
@@ -4954,9 +4954,6 @@ std::map<std::string, std::string> PeerConnection::GetTransportNamesByMid()
 std::map<std::string, cricket::TransportStats>
 PeerConnection::GetTransportStatsByNames(
     const std::set<std::string>& transport_names) {
-  if (!transport_controller_) {
-    return {};
-  }
   if (!network_thread()->IsCurrent()) {
     return network_thread()
         ->Invoke<std::map<std::string, cricket::TransportStats>>(
