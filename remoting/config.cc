05d46a1d186164a3a70c888869dd98a626c1c0ef
holomarkus <m.webel@holo-light.com>
2019-05-09 18:06:33 +0200

    Got rid of erroneously tracked remoting submodule

--- remoting/config.cc
+++
@@ -1,8 +0,0 @@
-#include "config.h"
-
-//TODO: this should return expected because parsing can fail or the config can be
-//full of shit
-Config Config::from_file(absl::string_view file_path) {
-  // TODO: something meaningful
-  return Config{};
-}
