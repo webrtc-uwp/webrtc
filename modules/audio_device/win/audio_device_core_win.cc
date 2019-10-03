50a63ef928601145646c18074c5d89884107974d
Halid Cisse <halidoucse@gmail.com>
2019-10-03 11:15:52 +0100

    Support 7 channels devices

--- modules/audio_device/win/audio_device_core_win.cc
+++ modules/audio_device/win/audio_device_core_win.cc
@@ -491,6 +491,7 @@ AudioDeviceWindowsCore::AudioDeviceWindowsCore()
   _recChannelsPrioList[0] = 2;  // stereo is prio 1
   _recChannelsPrioList[1] = 1;  // mono is prio 2
   _recChannelsPrioList[2] = 4;  // quad is prio 3
+  _recChannelsPrioList[3] = 7;  // azure kinect have 7
 
   // list of number of channels to use on playout side
   _playChannelsPrioList[0] = 2;  // stereo is prio 1
